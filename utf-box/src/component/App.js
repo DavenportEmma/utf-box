1037a0d316fcd257d54a4a3e3e5ddb57ae9c6289
ConorDavenport <cnrdavenport@gmail.com>
2020-05-26 17:53:43 +0100

    move App to components

---
+++ utf-box/src/component/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './../logo.svg';
+import './App.css';
+
+function App() {
+  return (  
+    <div><p>monospace</p>
+    <p>lllwwwww</p></div>
+  );
+}
+
+export default App;
