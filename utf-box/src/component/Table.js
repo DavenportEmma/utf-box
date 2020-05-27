e3e0d230cd5cd4a3da3f555e85f40c1fb2dfd87a
ConorDavenport <cnrdavenport@gmail.com>
2020-05-27 14:58:48 +0100

    add table component

---
+++ utf-box/src/component/Table.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import './Table.css'
+
+class Cell extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isHovered: false,
+      colour: 'black',
+      value: ''
+    }
+  }
+
+  mouseOverHandler() {
+    this.setState({
+      isHovered: true,
+      colour: 'black'
+    })
+  }
+
+  mouseOutHandler() {
+    this.setState({
+      isHovered: false,
+      colour: 'black'
+    })
+  }
+
+  clickHandler() {
+    this.setState({value: '┃'})
+  }
+
+  render() {
+    return (
+      <span 
+        className='cell'
+        onMouseOver={() => this.mouseOverHandler()}
+        onMouseOut={() => this.mouseOutHandler()}
+        onClick={() => this.clickHandler()}>
+        {this.state.value}
+      </span>
+    )
+  }
+}
+
+export default class Table extends React.Component {
+  renderCell(i) {
+    return <Cell key={i}/>
+  }
+
+  renderRow(n) {
+    const cols = [];
+    for (let i = 0; i < n; i++) {
+      cols.push(this.renderCell(i));
+    }
+    return cols;
+  }
+
+  renderTable(rn, cn) {
+    const rows = [];
+    for (let i = 0; i < rn; i++) {
+      rows.push(<div key={i} className='row'>{this.renderRow(cn)}</div>)
+    }
+    return rows;
+  }
+
+  render() {
+    const colNum = 50;
+    const rowNum = 25;
+
+    return (
+      <div className='table'>
+        {this.renderTable(rowNum, colNum)}
+      </div>
+    )
+  }
+}
